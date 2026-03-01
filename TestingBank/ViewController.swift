f97dde7c8f0e38beab5dd72287a8b04c534d4f7a
Anderson Valencia <andersson@Anderssons-MacBook-Pro.local>
2026-03-01 18:46:15 -0500

    Initial commit - TestingBank project setup

---
+++ TestingBank/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestingBank
+//
+//  Created by Andersson on 1/03/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
